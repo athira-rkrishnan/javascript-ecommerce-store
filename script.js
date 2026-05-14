b8c64b558ea10090ed0f4bc6df92b8770c29e3d2
Athira RK <athirarr764@gmail.com>
2026-05-14 18:38:34 +0530

    feat: add show/hide functionality for filter container on toggle and close button click at 480px breakpoint

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const toggleMenu = document.querySelector(".toggle-menu");
 const navigationMenu = document.querySelector(".nav-section ul");
+const filterToggle = document.querySelector(".filter-toggle");
+const filterContainer = document.querySelector(".filter-container");
+const closeFilter = document.querySelector(".close-filter");
 
 toggleMenu.addEventListener("click", () => {
     navigationMenu.classList.toggle("active");
 });
 
+filterToggle.addEventListener("click", () => {
+    filterContainer.classList.add("active");
+});
+
+closeFilter.addEventListener("click", () => {
+    filterContainer.classList.remove("active");
+});
 
