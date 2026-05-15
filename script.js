a1e1accc8141de4997f775835ded86be65f96312
Athira RK <athirarr764@gmail.com>
2026-05-15 15:32:08 +0530

    Implement fetchProducts function to retrieve products from dummyjson API

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const navigationMenu = document.querySelector(".nav-section ul");
 const filterToggle = document.querySelector(".filter-toggle");
 const filterContainer = document.querySelector(".filter-container");
 const closeFilter = document.querySelector(".close-filter");
+const productsListsContainer = document.querySelector(".products-lists-container");
+
 
 toggleMenu.addEventListener("click", () => {
     navigationMenu.classList.toggle("active");
@@ -15,4 +17,18 @@ filterToggle.addEventListener("click", () => {
 closeFilter.addEventListener("click", () => {
     filterContainer.classList.remove("active");
 });
+ 
+async function fetchProducts() {
+    try {
+        const response = await fetch("https://dummyjson.com/products");
+        console.log(response);
+        const products = await response.json();
+        console.log(products);
+    }
+    catch(error) {
+        console.log("Error fetching products:", error);
+    }
+}
+
+fetchProducts();
 
